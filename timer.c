c2ecb47281760e5d5d7214b681081689d052cabc
Elian Wu <elianwu@foxmail.com>
2024-08-27 00:57:45 +0800

    完善

--- timer.c
+++ timer.c
@@ -2,13 +2,14 @@
 #include "timer.h"
 #include "display.h"
 
+#define TIMS 45872 //大约50ms一次
+
 //初始化定时器0
 void Init_timer0()
 {
 	TMOD = 0x01; //设置定时器基础模式
-	int TIM = 45872; //大约50ms一次
-	TL0 = TIM;
-	TH0 = TIM >> 8;
+	TL0 = TIMS;
+	TH0 = TIMS >> 8;
 	EA = 1;
 	ET0 = 1;
 	TR0 = 1;
