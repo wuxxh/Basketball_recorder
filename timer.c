40546161a3227bc6f7f1c795a33d10a124ef1e86
Elian Wu <elianwu@foxmail.com>
2024-08-27 00:22:24 +0800

    新增timer.c,负责初始化定时器

--- timer.c
+++ timer.c
@@ -2,7 +2,14 @@
 #include "timer.h"
 #include "display.h"
 
-void Init_timer()
+//初始化定时器0
+void Init_timer0()
 {
-	TMOD = 0x01;
+	TMOD = 0x01; //设置定时器基础模式
+	int TIM = 45872; //大约50ms一次
+	TL0 = TIM;
+	TH0 = TIM >> 8;
+	EA = 1;
+	ET0 = 1;
+	TR0 = 1;
 }
