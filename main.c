0da40c06566626dbdec5276c9e70093d0c039202
Elian Wu <elianwu@foxmail.com>
2024-08-27 08:47:02 +0800

    新增注释

--- main.c
+++ main.c
@@ -5,31 +5,33 @@
 #include "timer.h"
 
 bit mode = 0;
-unsigned char sec=0,min=10,num=0,flag=0,q,w,mq,mw,fq,fw;
-//倒计时显示
+unsigned char sec = 0, min = 10, num = 0, flag = 0, q, w, mq, mw, fq, fw;
+// 倒计时显示
 void count_down_display()
 {
-	display(1,mw);
-	display(2,mq);
-	display(3,10);
-	display(4,w);
-	display(5,q);
-	display(7,fw);
-	display(8,fq);
+	display(1, mw);
+	display(2, mq);
+	display(3, 10);
+	display(4, w);
+	display(5, q);
+	display(7, fw);
+	display(8, fq);
 }
 
 void main()
 {
 	Init_timer0();
-	while(1){
-		if(key_s1==0)
+	while (1)
+	{
+		if (key_s1 == 0)
 			delay(5);
-		if(key_s1==0)
+		if (key_s1 == 0)
 		{
-			mode=~mode;
-			while(!key_s1);
+			mode = ~mode;
+			while (!key_s1)
+				;
 		}
-		if(mode)
+		if (mode)
 		{
 			count_down_display();
 		}
@@ -40,35 +42,28 @@ void main()
 	}
 }
 
-
 void time() interrupt 1
 {
-	TH0=(65535-45872)/256;
-	TL0=(65536-45872)%256;
+	TH0 = (65535 - 45872) / 256;
+	TL0 = (65536 - 45872) % 256;
 	num++;
-	if(num==20)
+	if (num == 20) // 20*50ms=1s
 	{
-		num=0;
+		num = 0;
 		flag++;
 		sec--;
-		q = sec%10;
-	    w = sec/10;
-		fw=flag/10;
-		fq=flag%10;
-
+		q = sec % 10;
+		w = sec / 10;
+		fw = flag / 10;
+		fq = flag % 10;
 	}
-	if(sec==0)
+	if (sec == 0)
 	{
-		sec=60;
+		sec = 60;
 		min--;
-		mq=min%10;
-		mw=min/10;
+		mq = min % 10;
+		mw = min / 10;
 	}
-	if(flag==24)
-		flag=0;
+	if (flag == 24)
+		flag = 0;
 }
-
-
-
-
-
